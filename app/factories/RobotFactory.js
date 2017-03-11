7d0a973463df9ba93c4d623540f6cce687263a1a
short880 <shortjustin880@gmail.com>
2017-03-11 16:59:11 -0600

    Building

--- app/factories/RobotFactory.js
+++ app/factories/RobotFactory.js
@@ -10,22 +10,76 @@ console.log("RobotFactory.js:");
 
 ****************************/
 
-app.factory('GetFactory', () => {
+app.factory('RobotFactory', () => {
 
-let Robot = () => {
-  this.name = null;
-  this.type = null;
-  this.health = null;
-  this.model = null;
+let Robot = {
+  name: null,
+  trait: null,
+  damage: 10
+}
+
+let HeavyClass = Object.create(Robot);
+HeavyClass.trait = "Bombard";
+HeavyClass.type = "HeavyClass";
+
+let AerialAssault = Object.create(Robot);
+AerialAssault.trait = "Missile Strike";
+AerialAssault.type = "AerialAssault";
+
+let LightClass = Object.create(Robot);
+LightClass.trait = "Speed Burst";
+LightClass.type = "LightClass";
+
+
+
+let RobotType = {};
+
+RobotType.HeavyOne = Object.create(HeavyClass);
+RobotType.HeavyOne.minHealth = 500;
+RobotType.HeavyOne.maxHealth = 800;
+RobotType.HeavyOne.baseDamage = 65;
+RobotType.HeavyOne.model = "HeavyOne";
+RobotType.Hacker.Image = "images/HeavyOne.jpg";
+
+RobotType.HeavyOne = Object.create(HeavyClass);
+RobotType.HeavyOne.minHealth = 550;
+RobotType.HeavyOne.maxHealth = 900;
+Robottype.HeavyOne.baseDamage = 50;
+RobotType.HeavyOne.model = "HeavyTwo";
+RobotType.Hacker.Image = "images/HeavyTwo.jpeg";
+
+RobotType.AerialOne = Object.create(AerialAssault);
+RobotType.AerialOne.minHealth = 400;
+RobotType.AerialOne.Maxhealth = 800;
+RobotType.AerialOne.baseDamage = 55;
+RobotType.AerialOne.Model = "AerialOne";
+RobotType.AerialOne.Image = "images/AerialOne.jpg";
+
+RobotType.AerialTwo = Object.create(AerialTwo);
+RobotType.AerialTwo.minHealth = 500;
+RobotType.AerialTwo.maxHealth = 950;
+RobotType.AerialOne.baseDamage = 65;
+RobotType.AerialTwo.model = "AerialTwo";
+RobotType.AerialTwo.Image = "images/AerialTwo.jpg";
+
+RobotType.LightOne = Object.create(LightClass);
+RobotType.LightOne.minhealth = 400;
+RobotType.LightOne.maxHealth = 750;
+RobotType.LightOne.baseDamage = 45;
+RobotType.LightOne.model = "LightOne";
+RobotType.LightOne.Image = "images/LightOne";
+
+RobotType.LightTwo = Object.create(LightClass);
+RobotType.LightTwo.minhealth = 400;
+RobotType.LightTwo.maxHealth = 750;
+RobotType.LightTwo.baseDamage = 45;
+RobotType.LightTwo.model = "LightOne";
+RobotType.LightTwo.Image = "images/LightOne";
+
+let getBot = (x) => {
+  return Bot[x];
 };
 
-var HeavyAssault = new Robot ();
-
-
-
-var AerialAssault = new Robot ();
-
-
-var LightAssault = new Robot ();
+return {getBot};
 
 });
